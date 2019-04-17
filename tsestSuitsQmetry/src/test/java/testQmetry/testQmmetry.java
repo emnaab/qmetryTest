e32c831f5146daec4353a229ecf7df5e6028c235
Emna.Bellaaj <Emna.Bellaaj@DEV12.phoenix.local>
2019-04-17 15:03:21 +0200

    ppppp

--- tsestSuitsQmetry/src/test/java/testQmetry/testQmmetry.java
+++ tsestSuitsQmetry/src/test/java/testQmetry/testQmmetry.java
@@ -1,14 +1,14 @@
-package testSuits;
+package testQmetry;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class testc1 {
+public class testQmmetry {
+
 
 	@Test
 	public void test() {
 		assertEquals(2,2);
 	}
-
 }
